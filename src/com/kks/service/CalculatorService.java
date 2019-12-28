1e989010dde5752a7a849f4b5f8a0ca6403dd53a
kamalkumarsahu <kamalkumarsahu8@gmail.com>
2019-12-28 17:03:48 +0530

    create service class and method

---
+++ src/com/kks/service/CalculatorService.java
@@ -0,0 +1,17 @@
+package com.kks.service;
+
+/**
+ * 
+ * @author KamalKumarSahu
+ *
+ */
+public class CalculatorService {
+
+	/**
+	 * 
+	 * @return interest
+	 */
+	public float calculate(){
+		return 0.0f;
+	}
+}
