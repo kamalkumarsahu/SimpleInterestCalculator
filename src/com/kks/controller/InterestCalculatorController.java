01fea6adea5864a76be6b017ed05bb6fd74dc73a
kamalkumarsahu <kamalkumarsahu8@gmail.com>
2019-12-28 16:54:30 +0530

    create skelton of contol method.

--- src/com/kks/controller/InterestCalculatorController.java
+++ src/com/kks/controller/InterestCalculatorController.java
@@ -9,4 +9,8 @@ public class InterestCalculatorController {
   
   // crate a method to control the which type of intrest it should the interest.
 
+	public float calculate(float amout,int year,float interest){
+		// calculate the result and return.
+		return 0.0f;
+	}
 }
